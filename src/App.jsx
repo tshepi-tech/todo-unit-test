04be77a45e3451fbf5b31ad98d36bd27503b02a3
tshepi-tech <tshepi.tech@gmail.com>
2022-04-04 18:32:11 +0200

    screen swtiches based on list length

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //NPM Packages
-import React from "react";
+import React, { useState } from "react";
 
 //Project files
 import ShoppingListScreen from "./components/ShoppingListScreen";
@@ -8,11 +8,14 @@ import WelcomeScreen from "./components/WelcomeScreen";
 //Project Files
 
 export default function App() {
+  //Local state
+  const [list, setList] = useState([]);
+
   return (
     <div className="App">
       <h1>Todo unit test</h1>
-      <ShoppingListScreen />
-      <WelcomeScreen />
+      {list.length > 0 && <ShoppingListScreen />}
+      {list.length === 0 && <WelcomeScreen />}
     </div>
   );
 }
