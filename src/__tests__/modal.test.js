d20ed702ca6415598ecd6e94a4dd60ae9f99b6a9
tshepi-tech <tshepi.tech@gmail.com>
2022-04-07 17:03:08 +0200

    initial modal test works

---
+++ src/__tests__/modal.test.js
@@ -0,0 +1,30 @@
+//NPM packages
+import { render, screen } from "@testing-library/react";
+
+//Project Files
+import Modal from "../components/Modal";
+import MockupModal from "../mockups/Mockupmodal";
+
+test("Modal opens up when ModalForm is passed in as a prop", () => {
+  //Arrange
+  const testModalState = [<MockupModal />, () => {}];
+  render(<Modal modalState={testModalState} />);
+
+  //Act
+  const titleElement = screen.queryByText("Mockup Modal");
+
+  //Asset
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("Don't open when you pass null on showModal", async () => {
+  //Arrange
+  const testCloseModalState = [null, () => {}];
+  render(<Modal modalState={testCloseModalState} />);
+
+  // Act
+  const titleElement = screen.queryByText("Mockup Modal");
+
+  // Assert
+  expect(titleElement).not.toBeInTheDocument();
+});
---
+++ src/mockups/Mockupmodal.jsx
@@ -0,0 +1,11 @@
+export default function MockupModal() {
+  return (
+    <div>
+      <h2>Mockup Modal</h2>
+      <p>
+        The modal is created to test if the Portal can render a modal inside as
+        a child prop
+      </p>
+    </div>
+  );
+}
