86a6497358a495e292086461f99cbf9f9fd8482d
tshepi-tech <tshepi.tech@gmail.com>
2022-04-08 15:26:31 +0200

    validatename function

---
+++ src/scripts/validateName.js
@@ -0,0 +1,7 @@
+export default function validateName(string) {
+  const exactString = string.trim();
+  const validation = exactString.length > 0;
+  const data = validation ? exactString : "";
+  const error = validation ? "" : "Please provide a product name, ex:Couch";
+  return { data: data, error: error };
+}
